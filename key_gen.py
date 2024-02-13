d38b713719c40524bc30e0d250729924cc7438d0
Davi Nascimento <157778682+JohnnyW23@users.noreply.github.com>
2024-02-13 10:33:16 -0300

    Update key_gen.py

--- key_gen.py
+++ key_gen.py
@@ -1,8 +1,6 @@
-from keyboard import read_hotkey
-from string import ascii_letters, digits
-from secrets import choice
-
 print("""
+\033[36mWELCOME!\033[m
+
 BACKSPACE: generates a random password
 ENTER: saves password to be stored in your file
 ESC: stores chosen passwords in your file and exits program
@@ -10,12 +8,25 @@ ESC: stores chosen passwords in your file and exits program
 
 
 def gen_key(length=12):
+    '''
+    -> Generates a random password using uppers, lowers, digits and symbols
+    :param length: Standard password length is defined as 12
+    :return: Returns a randomly generated password
+    '''
+    from string import ascii_letters, digits
+    from secrets import choice
     char = ascii_letters + digits + '!@#$%&+=-/~'
     password = ''.join(choice(char) for _ in range(length))
     return password
 
 
 def main():
+    '''
+    -> Fuction with keyboard module imported. Allows keys to execute commands.
+    Works with gen_key().
+    :return: Returns
+    '''
+    from keyboard import read_hotkey
     pass_gen = False
 
     while True:
@@ -31,7 +42,7 @@ def main():
                 with open("random_keys.txt", 'a') as f:
                     f.write(password + '\n')
 
-                print('{}Password saved ✓{}'.format('\033[1;32m', '\033[m'))
+                print('{}Password saved ✓{}\n'.format('\033[1;32m', '\033[m'))
                 pass_gen = False
 
             else:
@@ -39,7 +50,7 @@ def main():
 
         elif key == 'esc':
             print(f"""Saved passwords stored at chosen file.
-Exiting program.""")
+\033[36mExiting program. Thank you!\033[m""")
             break
 
 
