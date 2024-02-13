4af31962666f7c786203da3c14edcee04b0af610
Davi Nascimento <157778682+JohnnyW23@users.noreply.github.com>
2024-02-13 12:09:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import keygen
+
+print("""
+\033[36mWELCOME!\033[m
+
+BACKSPACE: generates a random password
+ENTER: saves password to be stored in your file
+ESC: stores chosen passwords in your file and exits program
+""")
+
+
+if __name__ == "__main__":
+    keygen.main()
